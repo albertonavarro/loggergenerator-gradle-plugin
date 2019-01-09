c91e6d28e4449299b5d9fcc53ae78162cbcdf498
Alberto Navarro <alberto.cartagena@gmail.com>
2019-01-09 23:28:35 +0100

    updated to 1.1.2 removed unused plugin

--- src/main/groovy/com/navid/loggergenerator/plugin/LoggerGeneratorExtension.groovy
+++ src/main/groovy/com/navid/loggergenerator/plugin/LoggerGeneratorExtension.groovy
@@ -2,7 +2,7 @@ package com.navid.loggergenerator.plugin;
 
 class LoggerGeneratorExtension {
 
-    static final DEFAULT_VERSION = "1.1.1"
+    static final DEFAULT_VERSION = "1.1.2"
 
     String executableVersion = DEFAULT_VERSION
 
