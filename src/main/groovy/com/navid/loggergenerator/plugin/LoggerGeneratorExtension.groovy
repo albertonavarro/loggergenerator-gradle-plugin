0762596cb3e390778d842ea6171902421057fbb3
Alberto Navarro <alberto.cartagena@gmail.com>
2019-01-10 13:51:09 +0100

    updated to 1.1.6 updated example

--- src/main/groovy/com/navid/loggergenerator/plugin/LoggerGeneratorExtension.groovy
+++ src/main/groovy/com/navid/loggergenerator/plugin/LoggerGeneratorExtension.groovy
@@ -2,7 +2,7 @@ package com.navid.loggergenerator.plugin;
 
 class LoggerGeneratorExtension {
 
-    static final DEFAULT_VERSION = "1.1.5"
+    static final DEFAULT_VERSION = "1.1.6"
 
     String executableVersion = DEFAULT_VERSION
 
