8da673c20adeec2413bf2ad3c1c24c01734ac3e5
Alberto Navarro <alberto.cartagena@gmail.com>
2019-01-10 22:59:11 +0100

    execution with more output now

--- src/main/groovy/com/navid/loggergenerator/plugin/LoggerGeneratorInvoker.groovy
+++ src/main/groovy/com/navid/loggergenerator/plugin/LoggerGeneratorInvoker.groovy
@@ -1,12 +1,12 @@
 package com.navid.loggergenerator.plugin
 
+import org.gradle.api.AntBuilder
+
 class LoggerGeneratorInvoker {
 
     static void invoke(LoggerGeneratorTask loggerGenTask) {
         Map executionParameters = new HashMap<>();
-        executionParameters.put("failOnError", true)
         executionParameters.put("jar", loggerGenTask.getExecutableJars().getSingleFile());
-        executionParameters.put("fork", true);
         executionParameters.put("args",
                 "--input " + loggerGenTask.getInputFile() +
                         " --package " + loggerGenTask.getPackageName() +
@@ -17,7 +17,18 @@ class LoggerGeneratorInvoker {
                         " --compat-" + loggerGenTask.getCompat())
 
         try {
-            Object result = loggerGenTask.getAnt().invokeMethod("java", executionParameters)
+            loggerGenTask.project.javaexec {
+                main="-jar"
+                args = [ loggerGenTask.getExecutableJars().getSingleFile(),
+                        "--input" , loggerGenTask.getInputFile() ,
+                                "--package" , loggerGenTask.getPackageName() ,
+                                "--codegen-output" , loggerGenTask.getCodegenOutput() ,
+                                "--class-name" , loggerGenTask.getClassName() ,
+                                "--html-name" , loggerGenTask.getHtmlName() ,
+                                "--html-output" , loggerGenTask.getHtmlOutput() ,
+                                "--compat-" + loggerGenTask.getCompat()
+                ]
+            }
         } catch( Exception e) {
             System.out.println("Command failed: " + executionParameters.toString())
             throw e
