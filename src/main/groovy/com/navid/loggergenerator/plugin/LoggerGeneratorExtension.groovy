f37df366c95052dd76435abb7d8c5cf5b8f8ffec
Alberto Navarro <alberto.cartagena@gmail.com>
2019-01-22 20:36:46 +0100

    updating to tool version 1.3.0

--- src/main/groovy/com/navid/loggergenerator/plugin/LoggerGeneratorExtension.groovy
+++ src/main/groovy/com/navid/loggergenerator/plugin/LoggerGeneratorExtension.groovy
@@ -2,7 +2,7 @@ package com.navid.loggergenerator.plugin;
 
 class LoggerGeneratorExtension {
 
-    static final DEFAULT_VERSION = "1.2.1"
+    static final DEFAULT_VERSION = "1.3.0"
 
     String executableVersion = DEFAULT_VERSION
 
