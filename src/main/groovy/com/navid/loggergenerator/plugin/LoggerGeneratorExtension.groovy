948a2de47461775e61f1b85cd4b841d90a13f3d2
Alberto Navarro <alberto.cartagena@gmail.com>
2019-03-17 15:35:19 +0100

    updating to tool version 1.4.9 -> fixed extradata examples

--- src/main/groovy/com/navid/loggergenerator/plugin/LoggerGeneratorExtension.groovy
+++ src/main/groovy/com/navid/loggergenerator/plugin/LoggerGeneratorExtension.groovy
@@ -2,7 +2,7 @@ package com.navid.loggergenerator.plugin;
 
 class LoggerGeneratorExtension {
 
-    static final DEFAULT_VERSION = "1.4.0"
+    static final DEFAULT_VERSION = "1.4.1"
 
     String executableVersion = DEFAULT_VERSION
 
